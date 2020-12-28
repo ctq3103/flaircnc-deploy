d4bf79d035c0631360669a799898fa12a4ba4207
ctq3103 <56945989+ctq3103@users.noreply.github.com>
2020-12-28 09:03:47 +0700

    fix deploy

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 "use strict";
+// import dotenv from 'dotenv';
+// dotenv.config();
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -12,8 +14,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
@@ -22,7 +22,7 @@ const compression_1 = __importDefault(require("compression"));
 const apollo_server_express_1 = require("apollo-server-express");
 const graphql_1 = require("./graphql");
 const database_1 = require("./database");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 9000;
 const mount = (app) => __awaiter(void 0, void 0, void 0, function* () {
     const db = yield database_1.connectDatabase();
     app.use(body_parser_1.default.json({ limit: '2mb' }));
